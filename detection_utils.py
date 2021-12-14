11800043bbdd4f9dac711a64df015c0f68c51e9d
hvassard <hugo.vassard@insa-rouen.fr>
2021-12-14 16:17:44 +0100

    refactor code hugo : basis for gesture development

--- detection_utils.py
+++ detection_utils.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy as np
 from constants import *
+import mediapipe as mp
+
+
+#################################
+## THE 3 RECOGNITION FUNCTIONS ##
+#################################
 
 
 def get_user_game_posture(img):
@@ -23,5 +29,41 @@ def get_starting_gesture(img):
     """
     Return the gesture made by the hand on the image
     which is an element of POSSIBLE_GESTURES
+    Code obtained from https://google.github.io/mediapipe/solutions/hands
     """
     pass
+
+
+def get_landmarks(img):
+    """
+    Code obtained from https://google.github.io/mediapipe/solutions/hands
+    """
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_hands = mp.solutions.hands
+
+    with mp_hands.Hands(
+        model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as hands:
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        img.flags.writeable = False
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(img)
+
+        # Draw the hand annotations on the img.
+        img.flags.writeable = True
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    img,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style(),
+                )
+
+        # Flip the image horizontally for a selfie-view display.
+        return cv2.flip(img, 1)
--- main_hugo.py
+++ main_hugo.py
@@ -7,49 +7,29 @@ import mediapipe as mp
 
 
 def run_app():
-    """
-    Code obtained from https://google.github.io/mediapipe/solutions/hands
-    """
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
-    mp_hands = mp.solutions.hands
+    # define a video capture object (0 = default webcam)
+    vid = cv2.VideoCapture(0)
 
-    cap = cv2.VideoCapture(0)
-    with mp_hands.Hands(
-        model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5
-    ) as hands:
-        while cap.isOpened():
-            success, image = cap.read()
-            if not success:
-                print("Ignoring empty camera frame.")
-                # If loading a video, use 'break' instead of 'continue'.
-                continue
+    while True:
+        # Capture the video frame by frame
+        ret, frame = vid.read(0)
 
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = hands.process(image)
+        # Faire le traitement et les modifications d'images ici
+        # e.g. : frame = iu.write_score(frame)
+        frame = du.get_landmarks(frame)
 
-            # Draw the hand annotations on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        image,
-                        hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style(),
-                    )
+        # Display the resulting frame
+        cv2.imshow("frame", frame)
 
-            # Flip the image horizontally for a selfie-view display.
-            cv2.imshow("MediaPipe Hands", cv2.flip(image, 1))
-            # Press 'q' to quit
-            key = cv2.pollKey() & 0xFF
-            if key == ord("q"):
-                break
+        # Press 'q' to quit
+        key = cv2.pollKey() & 0xFF
+        if key == ord("q"):
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
