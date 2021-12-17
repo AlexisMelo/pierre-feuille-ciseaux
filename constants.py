6fa184df1e687e0aab8b29e649ce7831d6c5554c
hvassard <hugo.vassard@insa-rouen.fr>
2021-12-17 14:26:04 +0100

    refactor

--- constants.py
+++ constants.py
@@ -2,14 +2,14 @@
 PIERRE = "pierre"
 FEUILLE = "feuille"
 CISEAUX = "ciseaux"
-
 POSSIBLE_GAME_POSTURES = [PIERRE, FEUILLE, CISEAUX]
 
 ### GESTURES ###
 LAUNCH_GAME = "launch_game"
 STATISTICS = "statistics"
-
 POSSIBLE_GESTURES = [LAUNCH_GAME, STATISTICS]
+# Keep the 60 last images in memory to detect the gestures
+MEMORY_SIZE = 60
 
 ### WINNER ###
 PLAYER_WIN = "player_win"
